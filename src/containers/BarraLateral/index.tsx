00fa82552c4ebcede63d42ae426bc8e5ab0680bc
jonatan timoteo dos santos <jonatan_ts@hotmail.com>
2023-06-12 11:31:23 -0300

    barra lateral finalizada

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,3 +1,23 @@
-const BarraLateral = () => <aside>Barra lateral</aside>
+import FiltroCard from '../../components/FiltroCard'
+
+import * as S from './styles'
+
+const BarraLateral = () => {
+  return (
+    <S.Aside>
+      <div>
+        <S.Input type="text" placeholder="Buscar" />
+        <S.Filtros>
+          <FiltroCard />
+          <FiltroCard />
+          <FiltroCard />
+          <FiltroCard />
+          <FiltroCard />
+          <FiltroCard ativo />
+        </S.Filtros>
+      </div>
+    </S.Aside>
+  )
+}
 
 export default BarraLateral
